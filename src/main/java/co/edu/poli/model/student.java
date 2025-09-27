30a162b6593b1e57d6a897d4ebb17f1307ed5876
DJAngel973 <angel.djuancho@gmail.com>
2025-09-26 20:19:35 -0500

    feat: add student class to define attributes

---
+++ src/main/java/co/edu/poli/model/student.java
@@ -0,0 +1,48 @@
+package co.edu.poli.model;
+
+/**
+ * Represents a student with id, name, and email.
+ * */
+public class student {
+    private String id;
+    private String name;
+    private String email;
+
+    /**
+     * Default constructor.
+     * */
+    public student() {
+    }
+
+    /**
+     * Constructs a student with the given id, name, and email.
+     * @param id Student's identifier
+     * @param name Student's name
+     * @param email Student's email
+     * */
+    public student(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
