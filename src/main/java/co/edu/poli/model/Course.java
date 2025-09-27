863c2ed3264a43d0ebe8dbb6080516e9b08fc5ef
DJAngel973 <angel.djuancho@gmail.com>
2025-09-26 23:42:02 -0500

    feat: add CourseService class to create, list courses and search a course by its id

--- src/main/java/co/edu/poli/model/Course.java
+++ src/main/java/co/edu/poli/model/Course.java
@@ -6,17 +6,21 @@ package co.edu.poli.model;
 public class Course {
     private String id;
     private String name;
+    private Integer capacity;
 
     public Course() {}
 
-    public Course(String id, String name) {
+    public Course(String id, String name, Integer capacity) {
         this.id = id;
         this.name = name;
+        this.capacity = capacity;
     }
 
     public String getId() {return id;}
     public String getName() {return name;}
+    public Integer getCapacity() {return capacity;}
 
     public void setId(String id) {this.id = id;}
     public void setName(String name) {this.name = name;}
+    public void setCapacity(Integer capacity) {this.capacity = capacity;}
 }
---
+++ src/main/java/co/edu/poli/service/CourseService.java
@@ -0,0 +1,34 @@
+package co.edu.poli.service;
+
+import co.edu.poli.model.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseService {
+    private final List<Course> courses = new ArrayList<>();
+
+    public Course createCourse(String id, String name, Integer capacity) {
+        for (Course cur : courses) {
+            if (cur.getId().equals(id)) {
+                throw new IllegalArgumentException(String.format("El curso con el id %s ya existe", id));
+            }
+        }
+        Course course = new Course(id, name, capacity);
+        courses.add(course);
+        return course;
+    }
+
+    public List<Course> getAllCourses() {
+        return new ArrayList<>(courses);
+    }
+
+    public Course searchCourseById(String id) {
+        for (Course course : courses) {
+            if (course.getId().equals(id)){
+                return course;
+            }
+        }
+        throw new IllegalArgumentException(String.format("El curso con el id %s no existe.", id));
+    }
+}
