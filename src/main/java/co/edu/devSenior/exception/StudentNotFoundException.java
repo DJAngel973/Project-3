8d812a685942f5305c733231120a0a3c7fffd4d0
DJAngel973 <angel.djuancho@gmail.com>
2025-09-27 23:08:30 -0500

    docs: rename exception

--- src/main/java/co/edu/devSenior/exception/StudentNotFoundException.java
+++ src/main/java/co/edu/devSenior/exception/StudentNotFoundException.java
@@ -3,14 +3,14 @@ package co.edu.devSenior.exception;
 /**
  * Exception thrown when a student is not found in the system.
  */
-public class EstudianteNoEncontradoException extends RuntimeException {
+public class StudentNotFoundException extends RuntimeException {
 
     /**
      * Constructs a new EstudianteNoEncontradoException with the specified detail message.
      *
      * @param message the detail message explaining the reason for the exception
      */
-    public EstudianteNoEncontradoException(String message) {
+    public StudentNotFoundException(String message) {
         super(message);
     }
 }
